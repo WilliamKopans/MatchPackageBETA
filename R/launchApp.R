c009c1ca1f1becf37cf2d49066be7a7ba0059dfd
William Kopans <wkopans123@gmail.com>
2021-05-25 07:09:54 -0400

    Update launchApp.R

--- R/launchApp.R
+++ R/launchApp.R
@@ -18,31 +18,31 @@ launchApp <- function() {
   shinyApp(ui = shinyAppUI, server = shinyAppServer)
   #shiny::runApp()
   
-  tryCatch(
-    expr = {
-      CurrentDir <- getwd()
-      CurrentDir <- paste(CurrentDir, "/R/app.R", sep="")
-      shiny::runApp(CurrentDir)
-    },
-    error = function(e){ 
-      message('Caught an error!')
-      print(e)
-      print("WD:")
-      message(getwd())
-    },
-    warning = function(w){
-      message('Caught an warning!')
-      print(e)
-      print("WD:")
-      message(getwd())
-    },
-    finally = {
-      message('Something went wrong')
-      print(e)
-      print("WD:")
-      message(getwd())
-    }
-  )
+#  tryCatch(
+#    expr = {
+#      CurrentDir <- getwd()
+#      CurrentDir <- paste(CurrentDir, "/R/app.R", sep="")
+#      shiny::runApp(CurrentDir)
+#    },
+#    error = function(e){ 
+#      message('Caught an error!')
+#      print(e)
+#      print("WD:")
+#      message(getwd())
+#    },
+#    warning = function(w){
+#      message('Caught an warning!')
+#      print(e)
+#      print("WD:")
+#      message(getwd())
+#    },
+#    finally = {
+#      message('Something went wrong')
+#      print(e)
+#      print("WD:")
+#      message(getwd())
+#    }
+#  )
 
   
   
