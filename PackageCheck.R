cb3528609d40089f08f07d975399fca26802f293
William <wkopans123@gmail.com>
2022-01-02 22:03:02 -0500

    Update PackageCheck.R

--- PackageCheck.R
+++ PackageCheck.R
@@ -3,7 +3,7 @@
 #First checking that all of the other packages are installed.
 
 PackagesToCheck <- list("ggplot2","ggrepel", "rio", "astrochron", "ggiraph", "systemfonts",
-                        "shinyjs", "caret", "shinyFiles", "stringr", "data.table", 
+                        "shinyjs", "caret", "shinyFiles", "stringr", "data.table", "devtools",
                         "tibble", "shinythemes", "shinyWidgets", "stats", "utils", "dplyr", 
                         "ggplot2", "shiny", "plotly", "shinyFiles")
 
